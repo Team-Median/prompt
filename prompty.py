3151c218de714e1b7bcc2553f2950a93ce7b3dc0
mhendricks96 <mhendricks96@yahoo.com>
2021-08-11 12:26:56 -0700

    finished a good portion of flightclub scraping

--- prompty.py
+++ prompty.py
@@ -1,1 +1,9 @@
-test 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
+import pandas as pd
+
+browser = webdriver.Chrome
